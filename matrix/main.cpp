be78d42503d020f690ee485e42da1f00d173706b
Андрей Иоффе <andrei.ioffe@gmail.com>
2019-12-28 23:08:56 +0200

    second constructor

--- matrix/main.cpp
+++ matrix/main.cpp
@@ -6,11 +6,17 @@ using std::vector;
 
 class Matrix{
 private:
-
+    
 public:
+    int rows,cols;
     vector<vector<int> > arr;
 
 Matrix(); //конструктор по умолчанию, который создаёт матрицу с нулём строк и нулём столбцов
+/*
+    конструктор от двух целочисленных параметров:
+    num_rows и num_cols, — которые задают количество строк и столбцов матрицы соответственно
+*/
+Matrix(int rows, int cols);
 
 };
 
@@ -23,6 +29,12 @@ int main(int argc, const char * argv[]) {
 
 
 Matrix::Matrix(){ //конструктор по умолчанию, который создаёт матрицу с нулём строк и нулём столбцов
-    vector<vector<int> > arr(0, vector<int>(0));  // vector<vector<int> > a(n, vector<int>(m));
+    arr(0, vector<int>(0));  // vector<vector<int> > a(n, vector<int>(m));
 }
+Matrix::Matrix(int rows, int cols){
+    this->rows=rows;
+    this->cols=cols;
+    vector<vector<int> > arr(rows, vector<int>(cols));
+}
+
 
