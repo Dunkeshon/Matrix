f93b864fd96cb2e3d29b80345baa37b92d1e252d
Андрей Иоффе <andrei.ioffe@gmail.com>
2019-12-28 22:34:13 +0200

    Initial Commit

---
+++ matrix/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  matrix
+//
+//  Created by Андрей Иоффе on 28.12.2019.
+//  Copyright © 2019 Андрей Иоффе. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
