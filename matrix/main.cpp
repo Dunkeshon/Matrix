81f1bf902c631fa8fd9f6ad13b6d76428386dc96
Андрей Иоффе <andrei.ioffe@gmail.com>
2019-12-28 22:57:05 +0200

    default construcor

--- matrix/main.cpp
+++ matrix/main.cpp
@@ -4,9 +4,25 @@
 #include <vector>
 using std::vector;
 
+class Matrix{
+private:
+
+public:
+    vector<vector<int> > arr;
+
+Matrix(); //конструктор по умолчанию, который создаёт матрицу с нулём строк и нулём столбцов
+
+};
 
 int main(int argc, const char * argv[]) {
     
+
     
     return 0;
 }
+
+
+Matrix::Matrix(){ //конструктор по умолчанию, который создаёт матрицу с нулём строк и нулём столбцов
+    vector<vector<int> > arr(0, vector<int>(0));  // vector<vector<int> > a(n, vector<int>(m));
+}
+
